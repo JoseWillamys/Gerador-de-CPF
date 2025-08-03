622c8de9c6d40b9679c4484f86a0232d9c1e0684
José Willamys <willamysoad@gmail.com>
2025-08-02 21:10:26 -0300

    resumo: GeraCPF.js
    fiz uma classe para gerar novos cpf validos

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
+import GeraCPF from './modules/GeraCPF'
+
 import './assets/css/style.css';
+
+(function() {
+    const cpf = new GeraCPF();
+    const cpfGerado = document.querySelector('.cpf-gerado');
+    cpfGerado.innerHTML = cpf.geraNovoCpf();
+})();
