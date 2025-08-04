5d86e99f748b3500710456a0312f79a4e30f312c
José Willamys <willamysoad@gmail.com>
2025-08-04 08:47:58 -0300

    feito: função para gerar elementos
    fiz uma função para gerar elementos html, e fiz algumas alterações no css

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import GeraCPF from './modules/GeraCPF'
+import GeraCPF from './modules/GeraCPF';
+import GeraElementos from './modules/GeradorDeElementosHTML';
 
 import './assets/css/style.css';
 
 (function() {
-    const cpf = new GeraCPF();
-    const cpfGerado = document.querySelector('.cpf-gerado');
-    cpfGerado.innerHTML = cpf.geraNovoCpf();
+    // const cpf = new GeraCPF();
+    // const cpfGerado = document.querySelector('.cpf-gerado');
+    // cpfGerado.innerHTML = cpf.geraNovoCpf();
+    GeraElementos(GeraCPF);
 })();
---
+++ src/modules/GeradorDeElementosHTML.js
@@ -0,0 +1,21 @@
+export default function geraElementos (geraCPF) {
+    const cpfGerado = document.querySelector('.cpf-gerado');
+    const cpf = new geraCPF();
+
+    if(!cpf) {
+        console.error('O CPF não foi gerado.');
+        return;
+    } else if (!cpfGerado) {
+        console.error('Elemento DOM não encontrado.');
+        return;
+    }
+    
+    const p = document.createElement('p');
+    p.innerHTML = cpf.geraNovoCpf();
+    const button = document.createElement('button');
+    button.setAttribute('class', 'gerar');
+    button.innerHTML = 'Gerar novo CPF';
+
+    cpfGerado.appendChild(p);
+    cpfGerado.appendChild(button);
+}
